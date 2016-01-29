fc66749da175bb7b8562d9d2095ea996ad51d269
Adrian Perez <adperez@adperez.es>
2016-01-29 09:19:52 +0100

    Initial Commit

---
+++ MVC_Complete/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MVC_Complete
+//
+//  Created by Adrián Perez López on 29/1/16.
+//  Copyright © 2016 ADP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
